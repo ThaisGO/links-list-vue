cb7a1d70a12dddf1386784d45e7dc70d27cf1a5d
ThaisGO <thaisgo.viana@gmail.com>
2024-11-20 16:44:11 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+    <the-header title="RememberMe"></the-header>
+    <stored-resources :resources="storeResources"></stored-resources>
+</template>
+
+<script>
+import TheHeader from "./components/layouts/TheHeader.vue";
+import StoredResources from "./components/learning-resources/StoredResources.vue"
+
+export default {
+    components: {
+        StoredResources,
+        TheHeader
+    },
+
+    data() {
+        return {
+            storeResources: [
+                {
+                    id: 'official-giude',
+                    tittle: 'Oficial Guide',
+                    description: 'The official Vue JS Documentation',
+                    link: 'https://vuejs.org'
+                },
+                {
+                    id: 'google',
+                    tittle: 'Google',
+                    description: 'The official Internet search site',
+                    link: 'https://www.google.com'
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+    * {
+    box-sizing: border-box;
+    }
+
+    html {
+    font-family: 'Roboto', sans-serif;
+    }
+
+    body {
+    margin: 0;
+    }
+</style>
